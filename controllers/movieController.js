ced2d085f6b1a47a9b775ba00949af6e13a8ece6
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2025-01-23 16:16:58 +0100

    feat: show route completed

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -9,17 +9,50 @@ const index = (req, res, next) => {
       return next(new Error(err.message));
     }
     return res.status(200).json({
-        status: "success",
-        data: movies
-    })
+      status: "success",
+      data: movies,
+    });
   });
 };
 
 //show
-const show = (req, res) => {
-  res.json({
-    message: "Show dei film",
+const show = (req, res, next) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM `movies` WHERE id=?";
+  const sqlReviews = `
+  SELECT reviews.*
+  FROM reviews
+  JOIN movies
+  ON movies.id = reviews.movie_id
+  WHERE movies.id = ?`;
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) {
+      return next(new Error(err.message));
+    }
+    if (results.length === 0) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Movie not found",
+      });
+    }
+
+    //query reviews
+    connection.query(sqlReviews, [id], (err, reviews) => {
+      if (err) {
+        return next(new Error(err.message));
+      }
+
+      return res.status(200).json({
+        status: "success",
+        data: {
+          ...results[0],
+          reviews,
+        },
+      });
+    });
   });
 };
+
 
 module.exports = { index, show };
